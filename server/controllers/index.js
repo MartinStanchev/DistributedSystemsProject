682544eb8041fe17df6ed4bca6edc149d97ba8a9
MamoshiSE <gushasmoi@student.gu.se>
2018-11-21 23:41:01 +0100

    minior changes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -21,7 +21,7 @@ router.get('/oauth/redirect', (req, res) => {
     }
   }).then((response) => {
     const accessToken = response.data.access_token
-    res.redirect(`/welcome.html?access_token=${accessToken}`)
+    res.redirect(`/profile.html?access_token=${accessToken}`)
   })
 })
 
