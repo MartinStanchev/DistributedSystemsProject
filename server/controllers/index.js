4154bb4c9e63ad95d59d93601a834a12f49fd2df
MartinStanchev <stanchev.martin@gmail.com>
2018-11-22 08:33:34 +0100

    adding using srcml at /api/xml GET

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,16 +3,11 @@ var express = require('express');
 var router = express.Router()
 
 router.get('/api', function(req, res) {
-    res.json({"message": "Welcome to DistrubutedSystemsProject - backend!"});
+    res.json({"message": "Welcome to backend"});
 });
 
 // Insert routes below
-router.use('/api/gitrepository', require('./gitrepository'));
-
-router.use('/api/gitrepo', require('./gitrepo.controller'));
-router.use('/api/diagrams', require('./diagram.controller'));
-
-
+router.use('/api/xml', require('./xmlEncoder.js'));
 
 router.route('/*').get(function (req, res) {
     var relativeAppPath = req.app.get('appPath');
---
+++ server/controllers/xmlEncoder.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var shell = require('shelljs');
+
+
+router.get('/', function(req, res, next) {
+    var res_dir = shell.pwd() + '/resources';
+    
+    if(shell.ls('-A', res_dir)) {
+        shell.echo('dir exists');
+        if(shell.exec('srcml ' + res_dir + '/javaProject.zip -o ' + res_dir + '/q2.xml').code !== 0) {
+            shell.echo('Error: srcml command failed.');
+            shell.exit(1);
+        }
+
+    } else {
+        res.status('404').json({"message":"No file in directory resources"});
+    }
+});
+
+module.exports = router
