95bf209d6fda55d4a218a98c5693839fcce73b43
MartinStanchev <stanchev.martin@gmail.com>
2018-11-22 08:58:41 +0100

    Adding srcml lib in project folder and execution of srcml happens with it

--- server/controllers/xmlEncoder.js
+++ server/controllers/xmlEncoder.js
@@ -5,10 +5,10 @@ var shell = require('shelljs');
 
 router.get('/', function(req, res, next) {
     var res_dir = shell.pwd() + '/resources';
-    
+    var srcml = shell.pwd() + '/srcML/bin/srcml ';
     if(shell.ls('-A', res_dir)) {
         shell.echo('dir exists');
-        if(shell.exec('srcml ' + res_dir + '/javaProject.zip -o ' + res_dir + '/q2.xml').code !== 0) {
+        if(shell.exec( srcml + res_dir + '/javaProject.zip -o ' + res_dir + '/q2.xml').code !== 0) {
             shell.echo('Error: srcml command failed.');
             shell.exit(1);
         }
