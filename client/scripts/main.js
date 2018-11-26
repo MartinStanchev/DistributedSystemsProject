ba6f7421d58dd5c10beaac236ac1f6f59d29851f
jansson215 <33342850+jansson215@users.noreply.github.com>
2018-11-26 14:14:26 +0100

    Fix for dual scrollbars and console errors

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,13 +2,19 @@
 document.addEventListener('DOMContentLoaded', function () {
 
         // Load particles (particles div tag)
-        particleground(document.getElementById('particles'), {
-          dotColor: '#fff',
-          lineColor: '#fff'
-        });
+        if(document.getElementById('particles') !== null) {
+          particleground(document.getElementById('particles'), {
+            dotColor: '#fff',
+            lineColor: '#fff'
+          });
+        }
+
         // Load intro (intro div tag)
         var intro = document.getElementById('intro');
-        intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+        if(intro !== null) {
+          intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+        }
+        
 
 
     }, false);
